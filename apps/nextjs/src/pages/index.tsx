97d593d703545fe90e35ed53e3f31589d0a0d0b5
Nestor Mamani <nmamanipantoja@gmail.com>
2022-12-20 22:34:29 -0500

    feat: landing ready

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -22,12 +22,27 @@ const Home: NextPage = () => {
 
       <Header />
 
-      <main className="mx-auto flex w-full max-w-6xl flex-col items-center justify-center px-4 xl:px-0">
+      <main className="mx-auto mb-10 flex w-full max-w-6xl flex-col items-center justify-center px-4 xl:px-0">
         <Title />
 
         <div className="mt-16 flex w-full flex-col-reverse justify-between gap-8 md:mt-24 lg:flex-row lg:items-center">
           <Cards />
-          <Preview />
+          <div className="flex w-full flex-col gap-4">
+            <Preview />
+
+            <p>
+              ¿Necesitas una nueva solución?{" "}
+              <a
+                href="https://twitter.com/nestoredduardo"
+                target="_blank"
+                rel="noreferrer"
+                className="font-bold text-primary"
+              >
+                Escríbenos
+              </a>
+              , la implementamos y te llevas 3 meses libres de comisiones.
+            </p>
+          </div>
         </div>
       </main>
     </>
