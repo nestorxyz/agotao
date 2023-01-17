c8c36cf1b0dd2a8c49208f866bd68d681fe88b45
Nestor Mamani <nmamanipantoja@gmail.com>
2023-01-17 18:28:34 -0500

    title mobile ready

--- apps/web/src/modules/landing/components/Title.tsx
+++ apps/web/src/modules/landing/components/Title.tsx
@@ -1,59 +1,23 @@
-import { useState, useEffect } from "react";
-import { AnimatePresence, motion, Variants } from "framer-motion";
-
-const customers = ["proyecto", "startup", "empresa"];
-const variants: Variants = {
-  enter: () => {
-    return {
-      y: -20,
-      opacity: 0,
-    };
-  },
-  center: {
-    zIndex: 1,
-    y: 0,
-    opacity: 1,
-  },
-  exit: () => {
-    return {
-      zIndex: 0,
-      opacity: 0,
-    };
-  },
-};
+import { Button } from "@/shared/components";
 
 export const Title: React.FC = () => {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    setTimeout(() => {
-      let next = index + 1;
-      if (next === customers.length) {
-        next = 0;
-      }
-      setIndex(next);
-    }, 3 * 1200);
-  }, [index, setIndex]);
-
   return (
-    <div className="relative my-4 max-w-[800px] text-center text-4xl font-extrabold sm:text-5xl md:text-6xl">
-      Acepta pagos en tu
-      <AnimatePresence>
-        <motion.h1
-          key={customers[index]}
-          className="absolute left-0 right-0 bg-gradient-to-r from-[#FF00B4] to-primary bg-clip-text py-2 text-transparent md:mt-2"
-          variants={variants}
-          initial="enter"
-          animate="center"
-          exit="exit"
-          transition={{
-            y: { type: "spring", stiffness: 300, damping: 200 },
-            opacity: { duration: 0.5 },
-          }}
-        >
-          {customers[index]}
-        </motion.h1>
-      </AnimatePresence>
+    <div className="relative my-4 mt-16 max-w-[800px] space-y-5 md:mt-28">
+      <h1 className="text-center text-[40px] font-extrabold leading-[45px] sm:text-5xl md:text-6xl">
+        Acepta pagos en Perú desde tu sitio web
+      </h1>
+      <p className="text-center text-lg text-gray-500">
+        Con las APIs y pasarela de pagos de Agotao puedes integrar pagos en tu
+        sitio web en minutos.
+      </p>
+      <Button
+        filled
+        color="primary"
+        size="large"
+        className="mx-auto w-fit px-6"
+      >
+        Crea tu cuenta
+      </Button>
     </div>
   );
 };
--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -10,7 +10,19 @@ module.exports = {
     extend: {
       colors: {
         black: "#1E1E1E",
-        primary: "#694FFF",
+        primary: {
+          DEFAULT: "#694fff",
+          50: "#f3f2ff",
+          100: "#e8e8ff",
+          200: "#d4d4ff",
+          300: "#b6b1ff",
+          400: "#9185ff",
+          500: "#694fff",
+          600: "#5a30f7",
+          700: "#4c1ee3",
+          800: "#3f18bf",
+          900: "#35169c",
+        },
       },
     },
   },
