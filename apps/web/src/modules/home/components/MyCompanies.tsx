ae38aef861018e64c3dc3c29a5ead2430008abe8
Nestor Mamani <nmamanipantoja@gmail.com>
2023-02-01 13:44:42 -0500

    fix create company btn

--- apps/web/src/modules/home/components/MyCompanies.tsx
+++ apps/web/src/modules/home/components/MyCompanies.tsx
@@ -1,25 +1,3 @@
-// Services
-import { trpc } from "@/utils/trpc";
-
-// Components
-import { CreateCompanyButton } from "@/modules/home/components";
-import { CompanyInfo } from "./CompanyInfo";
-
 export const MyCompanies: React.FC = () => {
-  const { data, refetch, isLoading } =
-    trpc.web.dashboard.getCompanies.useQuery();
-
-  return (
-    <div className="w-full">
-      {isLoading && <p>Cargando...</p>}
-
-      {data?.result === null && (
-        <CreateCompanyButton onCreated={() => refetch()} />
-      )}
-
-      {data?.result.company && (
-        <CompanyInfo company={data.result.company} refetch={refetch} />
-      )}
-    </div>
-  );
+  return <div className="w-full"></div>;
 };
