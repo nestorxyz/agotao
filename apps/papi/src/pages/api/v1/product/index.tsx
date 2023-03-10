e3681aca0a065ce5a86674bc747ae4e90f5b4379
Nestor Mamani <nmamanipantoja@gmail.com>
2023-03-10 12:26:10 -0500

    feat: api to create products

---
+++ apps/papi/src/pages/api/v1/product/index.tsx
@@ -0,0 +1,103 @@
+import Joi from "joi";
+import boom from "@hapi/boom";
+import { prisma } from "@acme/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import withJoi from "@/lib/withJoi";
+
+const bodySchema = Joi.object({
+  name: Joi.string().required(),
+  price: Joi.number().required(),
+  image: Joi.string().required().uri(),
+  metadata: Joi.object().optional(),
+});
+
+interface ValidatedRequest extends NextApiRequest {
+  body: {
+    name: string;
+    price: number;
+    image: string;
+    metadata: Record<string, unknown>;
+  };
+}
+
+const handler = async (req: ValidatedRequest, res: NextApiResponse) => {
+  try {
+    // Authorization: Bearer sk_live_123
+    const bearerHeader = req.headers.authorization;
+
+    if (typeof bearerHeader !== "string") {
+      throw boom.unauthorized("Invalid Token");
+    }
+
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+
+    if (!bearerToken) {
+      throw boom.unauthorized("Invalid Token");
+    }
+
+    if (req.method == "POST") {
+      const company = await prisma.company.findUnique({
+        where: {
+          sk_live: bearerToken,
+        },
+      });
+
+      if (!company) {
+        throw boom.unauthorized("Invalid Token");
+      }
+
+      // Logic
+      const {
+        body: { name, price, image, metadata },
+      } = req;
+
+      const product = await prisma.product.create({
+        data: {
+          company_id: company.id,
+          name,
+          price,
+          image,
+          created_by: company.admin_id,
+        },
+        select: {
+          id: true,
+          name: true,
+          price: true,
+          image: true,
+        },
+      });
+
+      return res.status(201).json({
+        product,
+        metadata,
+      });
+    } else {
+      throw boom.methodNotAllowed("Method Not Allowed");
+    }
+
+    // eslint-disable-next-line
+  } catch (err: any) {
+    console.log(err);
+
+    if (err.isBoom) {
+      const {
+        output: { statusCode, payload },
+      } = err;
+
+      return res.status(statusCode).json(payload);
+    }
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export default withJoi(
+  {
+    body: bodySchema,
+  },
+  handler,
+);
